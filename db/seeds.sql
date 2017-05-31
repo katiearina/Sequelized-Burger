10a3d65ef30695a3e3256ace14aaa4cfa1fc8ef7
Katie <katiearina@gmail.com>
2017-05-30 21:57:03 -0400

    :melon::tangerine::tomato: Removed seeds.sql file.

--- db/seeds.sql
+++
@@ -1,7 +0,0 @@
--- Starter Burgers for Table
--- INSERT INTO burgers (burger_name) VALUES ("The Bellsprout: Tofu burger with heirloom tomato slices, vegan garlic aioli, mixed greens, and fresh sprouts");
--- INSERT INTO burgers (burger_name) VALUES ("The Pikachu: Spicy black bean burger with curry aioli, pickled red onions, pickled jalapeños, and fresh pineapple salsa");
--- INSERT INTO burgers (burger_name) VALUES ("The Venusaur: Classic beef burger with pickled red onions, fresh arugula, goat cheese, and beet ketchup");
--- INSERT INTO burgers (burger_name) VALUES ("The Meowth: Beer-battered cod with mixed greens, pickled green tomato slices, garlic aioli, fresh dill, and a lemon wedge");
--- INSERT INTO burgers (burger_name) VALUES ("The Vileplume: Balsamic-marinated portobello mushroom cap with fresh mozzarella, fresh basil, heirloom tomato slices, and roasted red pepper aioli");
--- INSERT INTO burgers (burger_name) VALUES ("The Zapdos: Grilled chicken breast with housemade buffalo sauce, mixed greens, pickled red onions, garlic aioli, and blue cheese crumbles");
